9e413fe490cfa2f2b2ee2e0d9100811c42b103ff
connection579 <wlswn4118@naver.com>
2020-11-14 17:41:10 +0900

    백준 2588 문제 곱셉 연습

---
+++ src/tset2588/Ex2.java
@@ -0,0 +1,22 @@
+package tset2588;
+
+import java.util.Scanner;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int A = in.nextInt();
+		int B = in.nextInt();
+		
+		in.close();
+		
+		System.out.println(A*(B%10));
+		System.out.println(A*(B%100/10));
+		System.out.println(A*(B%100));
+		System.out.println(A*B);
+		
+	}
+
+}
